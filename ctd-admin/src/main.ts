755bfb7b8e5e0ad276a5cdea1cf4b4a4ebe240e2
ZHANGXU\illeg <illegals0001@gmail.com>
2024-08-16 14:01:00 +0800

    配置unocss

--- ctd-admin/src/main.ts
+++ ctd-admin/src/main.ts
@@ -1,8 +1,9 @@
-import './assets/main.css'
+import '@/assets/main.css'
+import 'virtual:uno.css'
 
 import { createApp } from 'vue'
+import App from '@/App.vue'
 
-import App from './App.vue'
 import router from '@/router'
 import store from '@/stores'
 import i18n from '@/locales'
--- ctd-admin/vite.config.ts
+++ ctd-admin/vite.config.ts
@@ -4,10 +4,11 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
+import UnoCSS from 'unocss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), VueI18nPlugin({})],
+  plugins: [vue(), vueDevTools(), VueI18nPlugin({}), UnoCSS()],
   resolve: {
     alias: {
       '~': fileURLToPath(new URL('./', import.meta.url)),
