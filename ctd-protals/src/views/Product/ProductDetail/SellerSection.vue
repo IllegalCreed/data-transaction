40f894ef12c75fcfe4afab94bf0876bbf371ef2a
ZHANGXU\illeg <illegals0001@gmail.com>
2024-09-13 15:09:16 +0800

    商家信息响应式布局

--- ctd-protals/src/views/Product/ProductDetail/SellerSection.vue
+++ ctd-protals/src/views/Product/ProductDetail/SellerSection.vue
@@ -1,20 +1,20 @@
 <template>
   <div class="seller-section-root-container">
     <el-card body-class="seller-body-container" v-if="sellerInfo">
-      <img :src="sellerInfo.logo" w-16 h-16 rounded-full mr-4 />
+      <img :src="sellerInfo.logo" />
       <div flex flex-col flex-1 min-w-0>
         <span text-lg font-bold mb-2>{{ sellerInfo.name }}</span>
-        <div flex flex-row items-center space-x-2>
+        <div flex flex-row flex-wrap items-center gap-2>
           <el-tag v-for="(tag, index) in sellerInfo.tags" :key="index" type="info">{{
             tag
           }}</el-tag>
         </div>
       </div>
-      <el-button type="primary" @click="viewSellerDetails">查看详情</el-button>
+      <el-button class="detail-btn" type="primary" @click="viewSellerDetails">查看详情</el-button>
     </el-card>
 
     <!-- 商家统计信息 -->
-    <div v-if="sellerStats" flex justify-around my-10>
+    <div v-if="sellerStats" class="stats-container">
       <div v-for="(stat, index) in sellerStats" :key="index" flex flex-col items-center>
         <span text-orange-500 block text-2xl font-bold mb-4>{{ stat.value }}</span>
         <span text-gray-500 text-sm>{{ stat.title }}</span>
@@ -71,7 +71,7 @@ const fetchRichTextContent = async (id: string) => {
   // 模拟 API 请求
   const rawContent = `
     <p>智能科技有限公司是一家专注于大数据和人工智能领域的创新企业，致力于为客户提供高效的解决方案。</p>
-    <img src="https://via.placeholder.com/600x400?text=About" alt="关于商家的图片">
+    <img src="https://via.placeholder.com/600x400?text=About" alt="关于商家的图片" style="width:100%">
     <p>公司成立于2010年，已累计服务超过1000家企业，覆盖多个行业，包括金融、医疗、零售等。</p>
   `
   richTextContent.value = DOMPurify.sanitize(rawContent)
@@ -91,11 +91,35 @@ onMounted(() => {
 <style scoped lang="scss">
 .seller-section-root-container {
   @apply flex flex-col;
+
+  img {
+    @apply w-16 h-16 rounded-full mr-4;
+  }
+
+  .stats-container {
+    @apply grid grid-cols-4 gap-4 my-10;
+  }
+
+  @media (max-width: 40rem) {
+    img {
+      @apply hidden;
+    }
+    .detail-btn {
+      @apply hidden;
+    }
+    .stats-container {
+      @apply grid-cols-2;
+    }
+  }
 }
 
 .wrap {
   :global(.seller-body-container) {
     @apply flex flex-row items-center;
+
+    @media (max-width: 40rem) {
+      @apply cursor-pointer;
+    }
   }
 }
 </style>
