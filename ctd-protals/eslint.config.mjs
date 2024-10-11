9d132d920868892a9fe8fd3d0c2afea6365ec45c
ZHANGXU\illeg <illegals0001@gmail.com>
2024-10-11 13:45:14 +0800

    修改eslint配置文件

--- ctd-protals/eslint.config.mjs
+++ ctd-protals/eslint.config.mjs
@@ -4,6 +4,11 @@ import skipFormattingConfig from '@vue/eslint-config-prettier/skip-formatting'
 import pluginCypress from 'eslint-plugin-cypress/flat'
 
 export default [
+  {
+    name: 'app/files-to-lint',
+    files: ['**/*.{js,mjs,jsx,ts,mts,tsx,vue}'],
+    ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**']
+  },
   ...pluginVue.configs['flat/essential'],
   ...vueTsEslintConfig(),
   skipFormattingConfig,
