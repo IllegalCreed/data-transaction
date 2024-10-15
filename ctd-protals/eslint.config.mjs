f91c90351f17407a6600c18122125f58a515bea6
ZHANGXU\illeg <illegals0001@gmail.com>
2024-10-15 16:05:02 +0800

    按照最新规范修改eslint配置文件

--- ctd-protals/eslint.config.mjs
+++ ctd-protals/eslint.config.mjs
@@ -1,16 +1,31 @@
 import pluginVue from 'eslint-plugin-vue'
 import vueTsEslintConfig from '@vue/eslint-config-typescript'
-import skipFormattingConfig from '@vue/eslint-config-prettier/skip-formatting'
+import pluginVitest from '@vitest/eslint-plugin'
 import pluginCypress from 'eslint-plugin-cypress/flat'
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
 
 export default [
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{js,mjs,jsx,ts,mts,tsx,vue}'],
+    files: ['**/*.{ts,mts,tsx,vue}']
+  },
+
+  {
+    name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**']
   },
+
   ...pluginVue.configs['flat/essential'],
   ...vueTsEslintConfig(),
-  skipFormattingConfig,
-  pluginCypress.configs.recommended
+
+  {
+    ...pluginVitest.configs.recommended,
+    files: ['src/**/__tests__/*']
+  },
+
+  {
+    ...pluginCypress.configs.recommended,
+    files: ['cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}', 'cypress/support/**/*.{js,ts,jsx,tsx}']
+  },
+  skipFormatting
 ]
