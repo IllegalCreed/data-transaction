8f615bf39f2a38a19eced01e835656ac8a3da9a9
ZHANGXU\illeg <illegals0001@gmail.com>
2024-10-17 13:36:28 +0800

    更新虚位以待占位图和mock数据

--- ctd-protals/src/constants/mockData/consult/team.ts
+++ ctd-protals/src/constants/mockData/consult/team.ts
@@ -5,28 +5,28 @@ export const teams: ITeam[] = [
     id: 1,
     title: '虚位以待',
     description: '期待与您的合作',
-    imageUrl: new URL('@/assets/mock/partnerLogo.png', import.meta.url).href,
+    imageUrl: new URL('@/assets/placeholder/teamDefault.jpg', import.meta.url).href,
     link: 'https://www.baidu.com'
   },
   {
     id: 2,
     title: '虚位以待',
     description: '期待与您的合作',
-    imageUrl: new URL('@/assets/mock/partnerLogo.png', import.meta.url).href,
+    imageUrl: new URL('@/assets/placeholder/teamDefault.jpg', import.meta.url).href,
     link: 'https://www.baidu.com'
   },
   {
     id: 3,
     title: '虚位以待',
     description: '期待与您的合作',
-    imageUrl: new URL('@/assets/mock/partnerLogo.png', import.meta.url).href,
+    imageUrl: new URL('@/assets/placeholder/teamDefault.jpg', import.meta.url).href,
     link: 'https://www.baidu.com'
   },
   {
     id: 4,
     title: '虚位以待',
     description: '期待与您的合作',
-    imageUrl: new URL('@/assets/mock/partnerLogo.png', import.meta.url).href,
+    imageUrl: new URL('@/assets/placeholder/teamDefault.jpg', import.meta.url).href,
     link: 'https://www.baidu.com'
   }
 ]
