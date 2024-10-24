730f9546a7db5ec05909712ee50ac04938172a11
ZHANGXU\illeg <illegals0001@gmail.com>
2024-10-24 16:16:43 +0800

    修改测试数据，创建服务器

--- ctd-protals/src/constants/mockData/home/banner.ts
+++ ctd-protals/src/constants/mockData/home/banner.ts
@@ -4,6 +4,6 @@ export const bannerList: IBanner[] = [
   {
     id: 1,
     imageUrl: new URL('@/assets/mock/banner.png', import.meta.url).href,
-    route: '/scene/1'
-  }
+    route: '/scene/2',
+  },
 ]
