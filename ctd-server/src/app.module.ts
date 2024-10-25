2ef21109dee27bccbdbd418c81bee3819856f0e0
ZHANGXU\illeg <illegals0001@gmail.com>
2024-10-25 16:12:14 +0800

    完成数据库链接

--- ctd-server/src/app.module.ts
+++ ctd-server/src/app.module.ts
@@ -1,5 +1,8 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+const nodeEnv = process.env.NODE_ENV || 'development';
 
 @Module({
   imports: [
@@ -8,9 +11,27 @@ import { ConfigModule } from '@nestjs/config';
       envFilePath: [
         '.env',
         '.env.local',
-        `.env.${process.env.NODE_ENV}`,
-        `.env.${process.env.NODE_ENV}.local`,
+        `.env.${nodeEnv}`,
+        `.env.${nodeEnv}.local`,
       ],
+    }),
+    // 动态模块, 可以动态注入配置
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => {
+        // 可以在此处编写调试代码
+        return {
+          type: 'mysql',
+          host: configService.get('DATABASE_HOST'),
+          port: configService.get('DATABASE_PORT'),
+          username: configService.get('DATABASE_USERNAME'),
+          password: configService.get('DATABASE_PASSWORD'),
+          database: configService.get('DATABASE_NAME'),
+          entities: [],
+          synchronize: configService.get('TYPEORM_SYNC'),
+        };
+      },
+      inject: [ConfigService],
     }),
   ],
   controllers: [],
--- ctd-server/src/main.ts
+++ ctd-server/src/main.ts
@@ -1,11 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // 全局启用验证管道
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(process.env.PORT ?? 3000);
+  const configService = app.get(ConfigService);
+  const port = configService.get('PORT');
+  await app.listen(port ?? 3000);
 }
 bootstrap();
