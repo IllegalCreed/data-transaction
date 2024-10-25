aeca343b91bb67c9929971e15aa8f5aa7a7ab0b2
ZHANGXU\illeg <illegals0001@gmail.com>
2024-10-25 11:37:37 +0800

    配置服务器eslint兼容最近版本配置文件。目前版本兼容crlf及lf

--- ctd-server/src/app.module.ts
+++ ctd-server/src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 全局模块, 不需要在其他模块引入
+      envFilePath: [
+        '.env',
+        '.env.local',
+        `.env.${process.env.NODE_ENV}`,
+        `.env.${process.env.NODE_ENV}.local`,
+      ],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
