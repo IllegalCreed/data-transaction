9e491ad51fe967fdef558e60ff0965fd9db34e3d
ZHANGXU\illeg <illegals0001@gmail.com>
2024-12-05 17:30:11 +0800

    修改表单验证

--- ctd-admin/src/views/Company/CompanyEdit/CompanyEditView.vue
+++ ctd-admin/src/views/Company/CompanyEdit/CompanyEditView.vue
@@ -67,9 +67,7 @@ import { PartnerTypes } from '@/constants/mapData/company'
 const id = useRouteParams<string>('id')
 const form = useTemplateRef<FormInstance>('form')
 const rules = reactive<FormRules<ICompanyDTO>>({
-  name: [{ required: true, message: '请输入公司名称', trigger: 'blur' }],
-  description: [{ required: true, message: '请输入公司简介', trigger: 'blur' }],
-  link: [{ required: true, message: '请输入公司链接', trigger: 'blur' }]
+  name: [{ required: true, message: '请输入公司名称', trigger: 'blur' }]
 })
 
 import { useCompanyStore } from '@/stores/modules/company'
