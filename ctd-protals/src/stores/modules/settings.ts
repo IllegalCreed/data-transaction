3c3fb2d070c23d1f008b9ac92bb84ea45f8a3bdc
ZHANGXU\illeg <illegals0001@gmail.com>
2024-12-13 09:53:09 +0800

    增加产品mock独立开关

--- ctd-protals/src/stores/modules/settings.ts
+++ ctd-protals/src/stores/modules/settings.ts
@@ -51,6 +51,7 @@ export const useSettingsStore = defineStore('settings', () => {
     { key: '账户', value: false, children: [] },
     { key: '首页', value: false, children: [] },
     { key: '场景', value: false, children: [] },
+    { key: '产品', value: false, children: [] },
   ])
 
   const findMockTreeValueByKey = (key: string) => {
