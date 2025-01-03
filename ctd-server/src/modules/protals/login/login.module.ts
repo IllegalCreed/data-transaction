52cc7aeeebdc48182ccce9a54aaa419bdca0df4f
ZHANGXU\illeg <illegals0001@gmail.com>
2025-01-03 11:27:54 +0800

    提取jwt到单独的模块

--- ctd-server/src/modules/protals/login/login.module.ts
+++ ctd-server/src/modules/protals/login/login.module.ts
@@ -1,30 +1,20 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { JwtModule } from '@nestjs/jwt';
-import { ConfigModule, ConfigService } from '@nestjs/config';
 import { LoginService } from './login.service';
 import { LoginController } from './login.controller';
 import { User } from 'src/entities/user.entity';
 import { LoginLog } from 'src/entities/login-log.entity';
 import { CaptchaModule } from 'src/modules/common/captcha/captcha.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   imports: [
     TypeOrmModule.forFeature([User, LoginLog]),
     CaptchaModule,
-    JwtModule.registerAsync({
-      imports: [ConfigModule],
-      inject: [ConfigService],
-      useFactory: async (configService: ConfigService) => ({
-        secret: configService.get<string>('JWT_SECRET', 'default_secret'),
-        signOptions: {
-          expiresIn: configService.get<string>('JWT_EXPIRES_IN', '3600s'),
-        },
-      }),
-    }),
+    AuthModule,
   ],
   controllers: [LoginController],
   providers: [LoginService],
-  exports: [LoginService, JwtModule],
+  exports: [LoginService],
 })
 export class LoginModule {}
--- ctd-server/src/modules/protals/user/user.module.ts
+++ ctd-server/src/modules/protals/user/user.module.ts
@@ -3,10 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from 'src/entities/user.entity';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
-import { LoginModule } from '../login/login.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User]), LoginModule],
+  imports: [TypeOrmModule.forFeature([User]), AuthModule],
   controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
