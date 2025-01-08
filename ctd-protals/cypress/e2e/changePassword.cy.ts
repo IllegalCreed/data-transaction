0c1c15317c7164efc6fa0eabe7b20ced4b1b9941
ZHANGXU\illeg <illegals0001@gmail.com>
2025-01-08 11:20:23 +0800

    完成修改密码全部测试用例编写

--- ctd-protals/cypress/e2e/changePassword.cy.ts
+++ ctd-protals/cypress/e2e/changePassword.cy.ts
@@ -98,7 +98,7 @@ describe('修改密码', () => {
     })
   })
 
-  it.only('在邮件验证码页面，验证码不合规，给予对应提示', () => {
+  it('在邮件验证码页面，验证码不合规，给予对应提示', () => {
     cy.visit('/mine/profile')
 
     // 点击修改密码按钮，进入修改密码页面
@@ -117,6 +117,141 @@ describe('修改密码', () => {
     enterVerificationCode('12345')
     cy.get('[data-testid="next-button"]').click()
     cy.contains('请输入完整的验证码').should('be.visible')
+  })
+
+  it('在身份验证页面，密码不合规，给予对应提示', () => {
+    cy.visit('/mine/profile')
+
+    cy.intercept(
+      'POST',
+      /\/(?:dev-api\/)?mailer\/send-verification-code\/auth/,
+    ).as('sendEmailRequest')
+
+    // 点击修改密码按钮，进入修改密码页面
+    cy.get('[data-testid="change-password-btn"]').click()
+    cy.get('.change-password-dialog-container').should('be.visible')
+
+    cy.wait('@sendEmailRequest').then(interception => {
+      // 断言请求响应成功
+      expect(interception.response?.statusCode).to.equal(201)
+
+      expect(interception.response?.body).to.have.property(
+        'msg',
+        'SEND_VERIFICATION_CODE_SUCCEED',
+      )
+
+      cy.getVerificationCode(Cypress.env('serverUrl'), testUser.email, 2)
+      cy.get<string>('@verificationCode').then(verificationCode => {
+        enterVerificationCode(verificationCode)
+        cy.get('[data-testid="next-button"]').click()
+        cy.contains('请输入登录密码').should('be.visible')
+
+        // 检测必填项
+        cy.get('[data-testid="current-password-input"]').clear()
+        cy.get('[data-testid="current-password-input"]').blur()
+        cy.contains('请输入密码').should('be.visible')
+
+        // 检测错误密码
+        cy.get('[data-testid="current-password-input"]').clear()
+        cy.get('[data-testid="current-password-input"]').type('WrongPwd')
+        cy.get('[data-testid="next-button"]').click()
+        cy.contains('验证密码失败').should('be.visible')
+        cy.contains('验证密码失败').should('not.exist')
+      })
+    })
+  })
+
+  it('设置新密码页面，密码不合规，给予对应提示', () => {
+    cy.visit('/mine/profile')
+
+    cy.intercept(
+      'POST',
+      /\/(?:dev-api\/)?mailer\/send-verification-code\/auth/,
+    ).as('sendEmailRequest')
+
+    // 点击修改密码按钮，进入修改密码页面
+    cy.get('[data-testid="change-password-btn"]').click()
+    cy.get('.change-password-dialog-container').should('be.visible')
+
+    cy.wait('@sendEmailRequest').then(interception => {
+      // 断言请求响应成功
+      expect(interception.response?.statusCode).to.equal(201)
+
+      expect(interception.response?.body).to.have.property(
+        'msg',
+        'SEND_VERIFICATION_CODE_SUCCEED',
+      )
+
+      cy.getVerificationCode(Cypress.env('serverUrl'), testUser.email, 2)
+      cy.get<string>('@verificationCode').then(verificationCode => {
+        enterVerificationCode(verificationCode)
+        cy.get('[data-testid="next-button"]').click()
+        cy.contains('请输入登录密码').should('be.visible')
+
+        enterVerificationPassword(testUser.password)
+        cy.get('[data-testid="next-button"]').click()
+        cy.contains('设置新密码').should('be.visible')
+
+        // 检测必填项
+        cy.get('[data-testid="new-password-input"]').clear()
+        cy.get('[data-testid="new-password-input"]').blur()
+        cy.contains('请输入密码').should('be.visible')
+        // 检测格式合规
+        const useCase = ['1234', '1234test', '1234testTEST', '1234TEST!']
+        for (const item of useCase) {
+          cy.get('[data-testid="new-password-input"]').clear()
+          cy.get('[data-testid="new-password-input"]').type(item)
+          cy.get('[data-testid="new-password-input"]').blur()
+          cy.contains(
+            '密码必须大于8位，且至少包含大小写字母数字及特殊字符',
+          ).should('be.visible')
+        }
+      })
+    })
+  })
+
+  it.only('设置新密码页面，确认密码不合规，给予对应提示', () => {
+    cy.visit('/mine/profile')
+
+    cy.intercept(
+      'POST',
+      /\/(?:dev-api\/)?mailer\/send-verification-code\/auth/,
+    ).as('sendEmailRequest')
+
+    // 点击修改密码按钮，进入修改密码页面
+    cy.get('[data-testid="change-password-btn"]').click()
+    cy.get('.change-password-dialog-container').should('be.visible')
+
+    cy.wait('@sendEmailRequest').then(interception => {
+      // 断言请求响应成功
+      expect(interception.response?.statusCode).to.equal(201)
+
+      expect(interception.response?.body).to.have.property(
+        'msg',
+        'SEND_VERIFICATION_CODE_SUCCEED',
+      )
+
+      cy.getVerificationCode(Cypress.env('serverUrl'), testUser.email, 2)
+      cy.get<string>('@verificationCode').then(verificationCode => {
+        enterVerificationCode(verificationCode)
+        cy.get('[data-testid="next-button"]').click()
+        cy.contains('请输入登录密码').should('be.visible')
+
+        enterVerificationPassword(testUser.password)
+        cy.get('[data-testid="next-button"]').click()
+        cy.contains('设置新密码').should('be.visible')
+
+        // 检测必填项
+        cy.get('[data-testid="confirm-password-input"]').clear()
+        cy.get('[data-testid="confirm-password-input"]').blur()
+        cy.contains('请再次确认密码').should('be.visible')
+        // 检测格式合规
+        cy.get('[data-testid="new-password-input"]').type('Password@123!')
+        cy.get('[data-testid="confirm-password-input"]').type('Password@123')
+        cy.get('[data-testid="confirm-password-input"]').blur()
+        cy.contains('请保证两次输入的密码一致').should('be.visible')
+      })
+    })
   })
 })
 
@@ -149,6 +284,6 @@ function getVerificationCodeAndResetPwd(email: string, password: string) {
     cy.get('[data-testid="next-button"]').click()
     cy.contains('您的密码已经修改完成').should('be.visible')
     cy.get('[data-testid="close-button"]').click()
-    cy.get('.change-password-dialog-container').should('not.visible')
+    cy.get('.change-password-dialog-container').should('not.exist')
   })
 }
